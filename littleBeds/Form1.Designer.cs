6a40d7b789da0646847405ca01d20067f95abdde
Matthew Sansom <matthewsansom@hotmail.co.uk>
2015-11-20 23:07:49 +0000

    First revision. Has had one change. Drawing the table myself instead of using somebody elses dll.

---
+++ littleBeds/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace littleBeds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Name = "dataGridView1";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ littleBeds/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Windows;
+
+
+
+namespace littleBeds
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            renderWindowData();
+
+
+        }
+  
+        public List<String> readFromXML(String xmlFilename, String xmlDescendant)
+        {
+            List<string> tempList = new List<string>();
+            XDocument xmlDoc = XDocument.Load(xmlFilename);
+            var currentDesc = xmlDoc.Descendants(xmlDescendant);
+            foreach (var xmlComponent in currentDesc)
+            {
+                string xmlToString = xmlComponent.Value.ToString();
+                tempList.Add(xmlToString);
+            }
+            return tempList;
+        }
+
+        public List<String> daysUntilFromList(List<string> listname)
+        {
+            List<string> tempList = new List<string>();
+            DateTime thisDay = DateTime.Today;
+            foreach (String stringDate in listname)
+            {
+                DateTime currentDate = DateTime.Parse(stringDate);
+                string daysUntil = (currentDate - thisDay).Days.ToString();
+                tempList.Add(daysUntil);
+            }
+            return tempList;
+
+        }
+
+
+        public string[,] buildAndReturnArray()
+        {
+            var nameOfEventList = readFromXML("config.xml", "NameOf");
+            var dateOfEventList = readFromXML("config.xml", "EventDate");
+            var daysUntilList = daysUntilFromList(dateOfEventList);
+            int arrayLength = nameOfEventList.Count;
+            string[,] convertedLists = new string[arrayLength, 3];
+            for (int i = 0; i < arrayLength; i++)
+            {
+                convertedLists[i,0] = nameOfEventList[i];
+            }
+            for (int i = 0; i < arrayLength; i++)
+            {
+                convertedLists[i,1] = dateOfEventList[i];
+            }
+            for (int i = 0; i < arrayLength; i++)
+            {
+                convertedLists[i,2] = daysUntilList[i];
+            }
+
+            return convertedLists;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            renderWindowData();
+        }
+        private void renderWindowData()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.Columns.Add("Event", "Event");
+            dataGridView1.Columns.Add("Date Of Event", "Date Of Event");
+            dataGridView1.Columns.Add("Days Until Event", "Days Until Event");
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeRows = false;
+         //   dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToOrderColumns = false;
+            var data = buildAndReturnArray();
+            var rowCount = data.GetLength(0);
+            var rowLength = data.GetLength(1);
+            for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex)
+            {
+                var row = new DataGridViewRow();
+
+                for (int columnIndex = 0; columnIndex < rowLength; ++columnIndex)
+                {
+                    row.Cells.Add(new DataGridViewTextBoxCell()
+                    {
+                        Value = data[rowIndex, columnIndex]
+                    });
+                }
+
+                dataGridView1.Rows.Add(row);
+            }
+        }
+    }
+}
